b7a7511d3b9ca2da32f441af741dee47f7c1ebd6
Jonash Marcelino <ijrmar7@gmail.com>
2022-07-02 15:09:02 +0800

    add the app.js and update the README.md

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Codecademy Mixed Message
-## By: Jonash Marcelino @ijrmar7
-Codecademy's Project: Mixed Message - a Node.js console app that generates random messages each time a user runs the program.
+
+## What is Mixed Message?
+a Node.js console app that generates random messages each time a user runs the program.
---
+++ app.js
@@ -0,0 +1,36 @@
+const mixedMessage = {
+    messages: {
+        jokes: ['to be filled'],
+        trivias: ['to be filled'],
+        advices: ['to be filled']
+    },
+    genRandNum: function(x) {
+        return Math.floor(Math.random() * x);
+    },
+    getMessage: function(name) {
+        const rand = this.genRandNum;
+        const mixedArr = [];
+        if(name) {
+            console.log("Hello " + name + '!');
+        } else {
+            console.log("Hello!");
+        }
+        for(obj in this.messages) {
+            const randNum = this.messages[obj].length;
+            switch(obj) {
+                case 'jokes':
+                    mixedArr.push("Your today's joke is: " + this.messages[obj][rand(randNum)]);
+                    break;
+                case 'trivias':
+                    mixedArr.push("Your today's trivia is: " + this.messages[obj][rand(randNum)]);
+                    break;
+                case 'advices':
+                    mixedArr.push("Your today's advice is: " + this.messages[obj][rand(randNum)]);
+                    break;
+            }
+        }
+        return mixedArr.join('\n');
+    }
+}
+const username = process.argv[2] || 'User';
+console.log(mixedMessage.getMessage(username));
